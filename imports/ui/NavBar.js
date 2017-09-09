8f0e224ec0536032529821e90d3f6e9e12ed54e4
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2017-09-09 16:47:44 -0400

    Added ui and SCSS

--- imports/ui/NavBar.js
+++ imports/ui/NavBar.js
@@ -5,18 +5,24 @@ export default class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-        <a className="navbar-brand" href={app}>Emsource</a>
+        <a className="navbar-brand brand" href={app}>Emsource</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+        <div className="collapse navbar-collapse">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
+            <li className="nav-item nav_items active">
               <a className="nav-link" href="#">Help <span className="sr-only">(current)</span></a>
             </li>
-            <li className="nav-item">
+            <li className="nav-item nav_items">
               <a className="nav-link" href="#">Services</a>
+            </li>
+            <li className="nav-item nav_items nav_items--border">
+              <a className="nav-link text-danger" href="#">Emergency</a>
+            </li>
+            <li className="nav-item nav_items nav_items--border">
+              <a className="nav-link text-success" href="#">Donate</a>
             </li>
           </ul>
           <form className="form-inline my-2 my-lg-0">
